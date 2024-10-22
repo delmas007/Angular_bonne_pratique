c56f7fa79a25b59ed118fba38685c7fa0737fdc9
delmas <azertyazerty1ze@gmail.com>
2024-10-22 08:59:55 +0000

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {MyFirstComponentComponent} from './components/my-first-component/my-first-component.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MyFirstComponentComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
